dae4afbc9ef02de1c4a5d8b95895e6ab9d287b2a
Art Cuddy <art.cuddy@dmgmedia.ie>
2023-01-13 14:16:24 +0000

    Add: category filter to post model

--- posts/serializers.py
+++ posts/serializers.py
@@ -15,13 +15,13 @@ class PostSerializer(serializers.ModelSerializer):
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.height > 2000:
+        if value.image.height > 4000:
             raise serializers.ValidationError(
-                'Image height larger than 2000px!'
+                'Image height larger than 4000px!'
             )
-        if value.image.width > 2000:
+        if value.image.width > 4000:
             raise serializers.ValidationError(
-                'Image width larger than 2000px!'
+                'Image width larger than 4000px!'
             )
         return value
 
@@ -45,4 +45,5 @@ class PostSerializer(serializers.ModelSerializer):
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'image_filter',
             'like_id', 'likes_count', 'comments_count',
+            'category_filter',
         ]
